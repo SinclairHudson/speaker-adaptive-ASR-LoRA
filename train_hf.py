e8db162ddee3bae7995ed61e11d9678bb3dda524
Sinclair Hudson <hudson.sinclair00@gmail.com>
2024-03-13 18:11:22 -0400

    proof of concept for loras in hf ecosystem

--- train_hf.py
+++ train_hf.py
@@ -6,6 +6,7 @@ import soundfile as sf
 import torch
 from datasets import load_dataset
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from peft import get_peft_model, LoraConfig
 
 # load pretrained model
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
@@ -21,10 +22,14 @@ print(librispeech_samples_ds[0]["file"])
 # pad input values and return pt tensor
 input_values = processor(audio_input, sampling_rate=sample_rate, return_tensors="pt").input_values
 
+
+lora_config = LoraConfig(init_lora_weights="gaussian", target_modules=["k_proj", "q_proj", "v_proj", "out_proj"])
+peft_model = get_peft_model(model, lora_config)
+
 # INFERENCE
 
 # retrieve logits & take argmax
-logits = model(input_values).logits
+logits = peft_model(input_values).logits
 predicted_ids = torch.argmax(logits, dim=-1)
 
 # transcribe
@@ -33,16 +38,21 @@ transcription = processor.decode(predicted_ids[0])
 # FINE-TUNE
 
 target_transcription = "A MAN SAID TO THE UNIVERSE I EXIST"
-print(transcription)
 
 # encode labels
 with processor.as_target_processor():
   labels = processor(target_transcription, return_tensors="pt").input_ids
 
+optimizer = torch.optim.AdamW(peft_model.parameters(), lr=1e-4)
+
+# this is good, we're only optimizing the lora here now
+for name, param in peft_model.named_parameters():
+  print(name, param.shape, param.requires_grad)
+
 # compute loss by passing labels
-loss = model(input_values, labels=labels).loss
-breakpoint()
+loss = peft_model(input_values, labels=labels).loss
 loss.backward()
-breakpoint()
+optimizer.step()
+optimizer.zero_grad()
 
 # Look at model._modules["wav2vec2"].encoder.layers[1].attention.k_proj.weight.grad
