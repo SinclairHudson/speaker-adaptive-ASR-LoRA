487e9f1f8d18c0291e916ff58b0d8ad294826c24
SinclairHudson <hudson.sinclair00@gmail.com>
2024-04-14 12:57:10 -0400

    filtering dataset and doing analytics

--- evaluate_librispeech_by_speaker.py
+++ evaluate_librispeech_by_speaker.py
@@ -6,62 +6,59 @@ from tqdm import tqdm
 import numpy as np
 import torch
 from evaluate import load
+import matplotlib.pyplot as plt
 wer_metric = load("wer")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-dataset_split = "train.clean.100"
+dataset_split = "test.clean"
+# for dataset_split in ['train.clean.100', 'train.clean.360', 'train.other.500', 'validation.clean', 'validation.other', 'test.clean', 'test.other']:
 full_dataset = load_dataset('librispeech_asr', split=dataset_split, cache_dir="/media/sinclair/M1/huggingface/datasets")
 # full_dataset = full_dataset.remove_columns(["chapter_id", "audio"])  # too much to store in memory, so drop these and load audio on the fly.
+# filtered_dataset = full_dataset.filter(lambda x: x['speaker_id'] == 6930)  # only evaluate on one speaker
 
+# breakpoint()
 processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h").to(device)
 
-dataset_by_speaker = {}
+def evaluate_per_speaker():
+    speaker_ids = np.load("data/librispeech_test_clean_speaker_ids.npy")
 
-# print("splitting the dataset by speaker")
-# for item in tqdm(full_dataset):
-    # if item['speaker_id'] in dataset_by_speaker:
-        # dataset_by_speaker[item['speaker_id']].append(item)
-    # else:
-        # dataset_by_speaker[item['speaker_id']] = [item]
+    data = np.zeros((0, 3))
+    for speaker_id in tqdm(speaker_ids):
+        speaker_dataset = full_dataset.filter(lambda x: x['speaker_id'] == speaker_id)
+        preds = []
+        gts = []
+        with torch.no_grad():
+            for item in tqdm(speaker_dataset):
+                input_values = processor(item["audio"]["array"],
+                                         sampling_rate=item["audio"]["sampling_rate"],
+                                         return_tensors="pt").input_values
 
-# Now we have a dictionary of speakers, with each speaker having a list of items
+                prediction = model(input_values.to(device))
+                pred_ids = np.argmax(prediction.logits.cpu(), axis=-1)
+                pred_str = processor.batch_decode(pred_ids)[0]
+                preds.append(pred_str)
+                gts.append(item["text"])
+        wer = wer_metric.compute(predictions=preds, references=gts)
+        data = np.vstack((data, np.array([speaker_id, len(speaker_dataset), wer])))
 
-# compute WER for each speaker
 
-# for speaker_id, speaker_dataset in tqdm(dataset_by_speaker.items()):
-    # total_wer = 0
-    # total_tokens = 0
-    # print("evaluating on speaker", speaker_id)
-    # for item in tqdm(speaker_dataset):
-        # audio_input, sample_rate = sf.read(item["file"])
-        # input_values = processor(audio_input, sampling_rate=sample_rate, return_tensors="pt").input_values
+    np.save("data/librispeech_test_clean_by_speaker.npy", data)
 
-        # prediction = model(input_values).logits
 
-        # wer = wer_metric.compute(predictions=prediction, references=item["sentence"])
+# plot data on two axes as a scatter_plot
+def plot_num_instances_by_wer():
+    data = np.load("data/librispeech_test_clean_by_speaker.npy")
+    print(data[:, 0])
+    plt.scatter(data[:, 1], data[:, 2])
+    plt.xlabel("Number of data instances")
+    plt.ylabel("Word Error Rate")
+    plt.title("Librispeech test.clean by speaker")
+    plt.tight_layout()
+    plt.show()
 
-predictions_by_speaker = {}
-
-with torch.no_grad():
-    for item in tqdm(full_dataset):
-        input_values = processor(item["audio"]["array"],
-                                 sampling_rate=item["audio"]["sampling_rate"],
-                                 return_tensors="pt").input_values
-
-        prediction = model(input_values.to(device))
-        pred_ids = np.argmax(prediction.logits.cpu(), axis=-1)
-        pred_str = processor.batch_decode(pred_ids)[0]
-
-        if item['speaker_id'] in predictions_by_speaker:
-            predictions_by_speaker[item['speaker_id']][0].append(pred_str)
-            predictions_by_speaker[item['speaker_id']][1].append(item['text'])
-        else:
-            predictions_by_speaker[item['speaker_id']] = [[pred_str], [item['text']]]
-
-for speaker in predictions_by_speaker:
-    wer = wer_metric.compute(predictions=predictions_by_speaker[speaker][0],
-                             references=predictions_by_speaker[speaker][1])
-    print("Speaker ", speaker, "WER ", wer)
+if __name__ == "__main__":
+    # evaluate_per_speaker()
+    plot_num_instances_by_wer()
 
